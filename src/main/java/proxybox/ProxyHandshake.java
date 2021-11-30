fdd0a0ece5cf90aa7dadd60e24053284f61ed007
Hugo Botas <hab12335@campus.fct.unl.pt>
2021-11-30 22:37:04 +0000

    fase 1 ok !!!

--- src/main/java/proxybox/ProxyHandshake.java
+++ src/main/java/proxybox/ProxyHandshake.java
@@ -57,6 +57,7 @@ public class ProxyHandshake {
         byte[] opaqueTicket = new byte[1024];
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         DataOutputStream dos = new DataOutputStream(baos);
+        DatagramPacket datagramPacket = makeSRTSPPacket(baos.toByteArray(), 3);
 
         return datagramPacket;
     }
@@ -64,9 +65,9 @@ public class ProxyHandshake {
     private DatagramPacket round3Packet() throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         DataOutputStream dos = new DataOutputStream(baos);
-
-
         DatagramPacket datagramPacket = makeSRTSPPacket(baos.toByteArray(), 3);
+
+
         dos.close();
         baos.close();
         return datagramPacket;
