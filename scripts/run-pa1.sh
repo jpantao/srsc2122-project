6c49f512af82e1e73f7d37f3f2003803d549e839
Hugo Botas <hab12335@campus.fct.unl.pt>
2021-12-02 02:31:24 +0000

    script missing a switch

--- scripts/run-pa1.sh
+++ scripts/run-pa1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 read -p "Open the network stream address udp://@224.7.7.7:7777 on vlc and press enter to start."
+# $(which vlc) udp://@224.7.7.7:7777
 
 mkdir -f "log"
 
@@ -11,7 +12,7 @@ echo 'Launching StreamingServerUDP...'
 (java -cp target/srsc-project-PA1.jar strserver.StreamingServerUDP > log/strserver.log) &
 sleep 3
 echo 'Launching ProxyBox...'
-java -cp target/srsc-project-PA1.jar proxybox.ProxyBox -user jpantao -password password -keystore keystores/this.keystore -proxyinfo config/proxybox.properties -movie $1 $2 | tee log/proxybox.log
+java -cp target/srsc-project-PA1.jar proxybox.ProxyBox -user jpantao -password password -keystore keystores/this.keystore -proxyinfo config/proxybox.properties -movie $1 -voucher $2 | tee log/proxybox.log
 
 
 
