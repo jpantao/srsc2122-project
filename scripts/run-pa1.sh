550ec4ae03b5411d797dad05fa5ef2a4ccbfedb5
Hugo Botas <hab12335@campus.fct.unl.pt>
2021-12-02 04:46:48 +0000

    script corrigido

--- scripts/run-pa1.sh
+++ scripts/run-pa1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-read -p "Open the network stream address udp://@224.7.7.7:7777 on vlc and press enter to start."
-# $(which vlc) udp://@224.7.7.7:7777
+#read -p "Open the network stream address udp://@224.7.7.7:7777 on vlc and press enter to start."
+($(which vlc) udp://@224.7.7.7:7777 >> /dev/null 2>&1) &
 
-mkdir -f "log"
+mkdir -p "log"
 
 echo 'Launching SignalingServer...'
 (java -cp target/srsc-project-PA1.jar sigserver.SignalingServer > log/sigserver.log) &
@@ -12,7 +12,7 @@ echo 'Launching StreamingServerUDP...'
 (java -cp target/srsc-project-PA1.jar strserver.StreamingServerUDP > log/strserver.log) &
 sleep 3
 echo 'Launching ProxyBox...'
-java -cp target/srsc-project-PA1.jar proxybox.ProxyBox -user jpantao -password password -keystore keystores/this.keystore -proxyinfo config/proxybox.properties -movie $1 -voucher $2 | tee log/proxybox.log
+java -cp target/srsc-project-PA1.jar proxybox.ProxyBox -user jpantao -password password -keystore keystores/proxybox.keystore -proxyinfo config/proxybox.properties -movie $1 -voucher $2 | tee log/proxybox.log
 
 
 
